08b5d35094a2804f46339dfe004ab4646ddc3586
zenaku16psp <pyaesone5p.s.p@gmail.com>
2026-02-25 16:56:42 +0630

    Update start.py

--- maythusharmusic/utils/inline/start.py
+++ maythusharmusic/utils/inline/start.py
@@ -3,14 +3,18 @@ from pyrogram.types import InlineKeyboardButton
 import config
 from maythusharmusic import app
 
-
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], 
+                url=f"https://t.me/{app.username}?startgroup=true",
+                style="primary" # အပြာရောင်
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_2"], 
+                url=config.SUPPORT_CHAT
+            ),
         ],
     ]
     return buttons
@@ -18,22 +22,27 @@ def start_panel(_):
 
 def private_panel(_):
     buttons = [
+        # ပထမတန်း (အပြာရောင်)
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["S_B_1"], 
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users",
+                style="primary" 
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        # ဒုတိယတန်း (အစိမ်းရောင်)
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style="success"),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style="success"),
         ],
+        # တတိယတန်း (အနီရောင်)
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style="danger"),
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source", style="danger"),
         ],
+        # စတုတ္ထတန်း (မီးခိုးရောင်/ပုံမှန်)
         [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
     ]
     return buttons
